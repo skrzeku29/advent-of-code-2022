269b209d232c2fe409ae77cfa185324677f92cd0
Szymon Skrzek <szymon.skrzek@bjss.com>
2022-12-02 13:13:07 +0000

    Moving execution instructions to readme

--- day1/day1.kts
+++ day1/day1.kts
@@ -1,12 +1,4 @@
 #!/usr/bin/env kscript
-/*The application is written in kotlin and is expected to be executed with kscript. Commands to install required depedencies are.
-brew install kotlin
-sdk install kscript
-first system argument can be supplied that is a filepath string leading to a file containing the data required to solve the problem
-to run the application run
-kscript day1.kts <optional filepath parameter | default is a file called test in the same directory as this file>
-or
-./day1.kts*/
 
 import java.io.File
 
