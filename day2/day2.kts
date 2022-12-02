c69b0773560d858a92fd3c86b4ddf4e8680973ca
Szymon Skrzek <szymon.skrzek@bjss.com>
2022-12-02 10:08:32 +0000

    2nd solution works

--- day2/day2.kts
+++ day2/day2.kts
@@ -21,8 +21,14 @@ val filepath = if (args.isEmpty()) "test" else args[0]
 val caloriesPerElf = retrieveSortedArrayOfCaloriesPerElf(filepath)
 
 println("Answer to task1: ${retrieveTask1Answer(caloriesPerElf)}")
+println("Answer to task2: ${retrieveTask2Answer(strategyGuide(filepath))}")
 
 fun retrieveTask1Answer(caloriesPerElf: List<Int>): Int {
+    // Retrieve first item in sorted array of calories per elf
+    return caloriesPerElf.sum()
+}
+
+fun retrieveTask2Answer(caloriesPerElf: List<Int>): Int {
     // Retrieve first item in sorted array of calories per elf
     return caloriesPerElf.sum()
 }
@@ -38,17 +44,59 @@ fun retrieveSortedArrayOfCaloriesPerElf(filepath: String): List<Int> {
     return games
 }
 
+fun strategyGuide(filepath: String): List<Int> {
+    val file = File(filepath).readLines()
+//        .map { line -> line
+//        .replace("X", "A").replace("Y", "B").replace("Z", "C") }
+    val games = ArrayList<Int>()
+    file.forEach() {
+            game -> games.add(getScoreFromArray(game))
+    }
+
+
+    return games
+}
+//x lose Y draw z win
+fun getCorrectPlayOnScenario(play: String, theirPlay: String): String {
+    if (play == "X")
+        return playLose(theirPlay)
+    if (play == "Z")
+        return playWinnable(theirPlay)
+    if (play == "Y")
+        return theirPlay
+    return play
+}
+//9177
+fun playWinnable(play: String): String {
+    if (play == "A")
+        return "B"
+    if (play == "B" )
+        return "C"
+//    if (play == "C")
+    return "A"
+}
+
+fun playLose(play: String): String {
+    if (play == "A")
+        return "C"
+    if (play == "B" )
+        return "A"
+//    if (play == "C")
+    return "B"
+}
+
 fun getScoreFromArray(game: String): Int {
     val split = game.split(" ")
-    if (split[0] == split[1])
-        return 3 + shapePlayed(split[1])
-    if (split[0] == "A" && split[1] == "B")
+    val correctPlay = getCorrectPlayOnScenario(split[1], split[0])
+    if (split[0] == correctPlay)
+        return 3 + shapePlayed(correctPlay)
+    if (split[0] == "A" && correctPlay == "B")
         return 6+2
-    if (split[0] == "B" && split[1] == "C")
+    if (split[0] == "B" && correctPlay == "C")
         return 6+3
-    if (split[0] == "C" && split[1] == "A")
+    if (split[0] == "C" && correctPlay == "A")
         return 6+1
-    return 0 + shapePlayed(split[1])
+    return 0 + shapePlayed(correctPlay)
 }
 
 fun shapePlayed(shape: String): Int {
